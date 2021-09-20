537c49c164b835b7e2d60205cfc0ee35144f51a5
remziusta <remziusta.mdbf15@iste.edu.tr>
2021-09-20 23:05:59 +0300

    Add config for swagger in customer-service.

--- customer-service/src/main/java/com/cas/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/cas/customerservice/CustomerServiceApplication.java
@@ -7,6 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
