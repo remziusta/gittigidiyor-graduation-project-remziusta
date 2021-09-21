0c5c1d3d0ab059b852bd56699ffea22b12899c68
remziusta <remziusta.mdbf15@iste.edu.tr>
2021-09-21 09:21:49 +0300

    Create score service module and add dependency in pom.xml.

---
+++ score-service/src/main/java/com/cas/scoreservice/ScoreServiceApplication.java
@@ -0,0 +1,15 @@
+package com.cas.scoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ScoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreServiceApplication.class, args);
+    }
+
+}
