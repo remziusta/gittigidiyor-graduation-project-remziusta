67c8673369774e2e51bd596a9618db0f20b139da
remziusta <remziusta.mdbf15@iste.edu.tr>
2021-09-21 12:27:36 +0300

    Add feign-client module.

---
+++ feign-client/src/test/java/com/cas/feignclient/FeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cas.feignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
